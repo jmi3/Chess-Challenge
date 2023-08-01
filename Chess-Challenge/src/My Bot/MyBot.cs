cc2badfd1f7944b8cfe8ccd3ba567f28cc5c2780
ManIReallyDontWantToMakeAnotherName <kcervenan157@gmail.com>
2023-08-01 14:32:15 +0200

    Added method MoveIsCheckmate

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,39 @@
 ﻿using ChessChallenge.API;
-
-namespace Bots;
-
+using ChessChallenge.Application;
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
+    bool MoveIsCheckmate(Board board, Move move)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
+    }
+    public Move Think(Board b, Timer timer)
+    {
+        Move[] AllMoves = b.GetLegalMoves();
+        foreach (Move move in AllMoves)
+        {
+            if (MoveIsCheckmate(b, move))
+            {
+                return move;
+            }
+            b.MakeMove(move);
+            Move[] AllMoves2 = b.GetLegalMoves();
+            bool leadsToLoss = false;
+            foreach (Move move2 in AllMoves2)
+            {
+                if (MoveIsCheckmate(b, move2))
+                {
+                    leadsToLoss = true;
+                    break;
+                }
+            }
+            if (leadsToLoss)
+            {
+                continue;
+            }
+            b.UndoMove(move);
+        }
     }
 }
