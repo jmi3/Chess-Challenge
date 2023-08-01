c9ce77214d21e27e11418d5be2ded4027705dfb9
jmi3 <jancervenan@gmail.com>
2023-08-01 14:43:10 +0200

    Added EvalMaterial method

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,6 +4,7 @@ public class MyBot : IChessBot
 {
     bool MoveIsCheckmate(Board board, Move move)
     {
+<<<<<<< Updated upstream
         board.MakeMove(move);
         bool isMate = board.IsInCheckmate();
         board.UndoMove(move);
@@ -36,4 +37,25 @@ public class MyBot : IChessBot
             b.UndoMove(move);
         }
     }
+    public float EvalMaterial(Board board, bool white)
+    {
+        int P = board.GetPieceList(PieceType.Pawn, true).Count - board.GetPieceList(PieceType.Pawn, false).Count;
+        int N = board.GetPieceList(PieceType.Knight, true).Count - board.GetPieceList(PieceType.Knight, false).Count;
+        int B = board.GetPieceList(PieceType.Bishop, true).Count - board.GetPieceList(PieceType.Bishop, false).Count;
+        int R = board.GetPieceList(PieceType.Rook, true).Count - board.GetPieceList(PieceType.Rook, false).Count;
+        int Q = board.GetPieceList(PieceType.Queen, true).Count - board.GetPieceList(PieceType.Queen, false).Count;
+        //int K = board.GetPieceList(PieceType.King, true).Count - board.GetPieceList(PieceType.King, false).Count;
+
+        //Multiply the material differences by their respective weights.
+        float result = (9 * Q) +
+            (5 * R) +
+            (3 * N) + (3.2f * B) + //N.B. the higher weighting for bishops!
+            (1 * P);
+
+        if (!white) { result *= -1; }
+
+        return result;
+    }
+
+
 }
